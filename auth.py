f17316cb5e98390bfba7e44978df255d62c77961
arpitarora <arpitarora@juniper.net>
2022-08-28 02:17:25 +0530

    Used render_template function to render basic HTML pages

--- auth.py
+++ auth.py
@@ -6,12 +6,12 @@ auth = Blueprint("auth", __name__)
 @auth.route("/signup")
 def signup():
 
-    return "This page is for the User signup"
+    return render_template("signup.html")
 
 @auth.route("/login")
 def login():
 
-    return "This page is for the User Login"
+    return render_template("login.html")
 
 @auth.route("/logout")
 def logout():
