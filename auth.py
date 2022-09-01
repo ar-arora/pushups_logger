d0b353ca65f7fb7bc64c592871534331a404ce73
arpitarora <arpitarora@juniper.net>
2022-09-01 21:52:16 +0530

    added signup_post function in auth.py for accepting fields from the sign up page

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, request, redirect
 
 auth = Blueprint("auth", __name__)
 
@@ -7,6 +7,17 @@ auth = Blueprint("auth", __name__)
 def signup():
 
     return render_template("signup.html")
+
+@auth.route("/signup", methods = ["POST"])
+def signup_post():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    print("Entered : ", name, email, password)
+
+    return redirect(url_for("auth.login"))
+
 
 @auth.route("/login")
 def login():
