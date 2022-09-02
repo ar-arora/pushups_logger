36780347374806ab3f3c6fa148c0e6ace7b6234c
arpitarora <arpitarora@juniper.net>
2022-09-02 13:38:13 +0530

    Added authentication with flask_login

--- auth.py
+++ auth.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, url_for, request, redirect
 from .models import User
 from . import db
-from werkzeug.security import generate_password_hash
-
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required
 
 auth = Blueprint("auth", __name__)
 
@@ -42,13 +42,20 @@ def login():
 def login_post():
     email = request.form.get("email")
     password = request.form.get("password")
+    remember = True if request.form.get("remember") else False
 
-    print("Entered values : ", email, password)
+    user = User.query.filter_by(email = email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        return redirect(url_for('auth.login'))
+
+    login_user(user, remember= remember)
 
     return redirect(url_for("main.profile"))
 
 
 @auth.route("/logout")
+@login_required
 def logout():
-
-    return "This page is for the User Logout"
+    logout_user()
+    return redirect(url_for("main.index"))
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, url_for
+from flask_login import login_required, current_user
 
 main = Blueprint("main", __name__)
 
@@ -8,9 +9,10 @@ def index():
     return render_template("index.html")
 
 @main.route("/profile")
+@login_required
 def profile():
 
-    return render_template("profile.html")
+    return render_template("profile.html", name = current_user.name)
 
 @main.route("/user_workouts")
 def user_workouts():
